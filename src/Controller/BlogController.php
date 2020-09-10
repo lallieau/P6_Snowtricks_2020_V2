8c8ae8dd154fc5379a875661daf40531d33fd8d8
lallie <lallie.audry@gmail.com>
2020-09-10 16:26:26 +0200

    create views of all methods

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,11 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
     public function index()
     {
         return $this->render('blog/index.html.twig');
+    }
+
+    public function add()
+    {
+        return $this->render('blog/add.html.twig');
+    }
+
+    public function show($url)
+    {
+        return $this->render('blog/show.html.twig', [
+            'slug' => $url
+        ]);
+    }
+
+    public function edit($id)
+    {
+        return $this->render('blog/edit.html.twig', [
+            'slug' => $id
+        ]);
+    }
+
+    public function remove($id)
+    {
+        return new Response('<h1>Supprimer l\'article ' . $id . '</h1>');
     }
 }
