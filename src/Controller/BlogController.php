8938d6ef8668d0564506994364e2286a1b62bf5d
lallie <lallie.audry@gmail.com>
2020-09-10 15:39:21 +0200

    create the first route to the home page

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BlogController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('blog/index.html.twig');
+    }
+}
