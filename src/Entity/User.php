ad7efc33ebd0b7803a07dd6183018e907c48ed86
lallie <lallie.audry@gmail.com>
2020-09-16 11:18:44 +0200

    feat(authentication): create a member account

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="Il existe déjà un compte avec cette adresse email")
  */
 class User implements UserInterface
 {
@@ -60,6 +62,11 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity="Article", mappedBy="user")
      */
     private $articles;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -241,5 +248,22 @@ class User implements UserInterface
         }
 
         return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getIsVerified(): ?bool
+    {
+        return $this->isVerified;
     }
 }
