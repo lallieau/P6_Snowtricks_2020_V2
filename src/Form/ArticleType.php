1b9bfd9b8bfee115272b5f12c847631b969ce7ea
lallie <lallie.audry@gmail.com>
2020-09-18 15:57:23 +0200

    experiment: start of modification of the form for adding an article (add image)

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,6 +18,13 @@ class ArticleType extends AbstractType
             ->add('title', TextType::class, ['required' => false])
             ->add('content', TextareaType::class, ['required' => false])
         ;
+
+        $builder
+            ->add('images', CollectionType::class, [
+               'entry_type' => ImageType::class,
+               'entry_options' => ['label' => false],
+                'allow_add' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
